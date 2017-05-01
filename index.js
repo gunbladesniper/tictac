86f9d5cf8eeacc279263dd73d2a0e874b05cca6c
Savaughn Jones <savaughnjones@gmail.com>
2017-05-01 15:24:34 -0700

    build out basic click functionality. game is playable for humans. working on game logic programming.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var player1=[];
-var player2=[];
-
+var player1={
+	num: 1,
+	name: 'Player One',
+	moves:[]
+};
+var player2={
+	num:2,
+	name: 'Player Two',
+	moves:[]
+};
+var turns = 0;
+var turnPlayer = player1;
 var wins ={
 	'123': true,
 	'456': true,
-	'789': true
+	'789': true,
+	'174': true,
+	'258': true,
+	'369': true,
+	'159': true,
+	'357': true
 }
 
 var boxes = document.querySelectorAll('.ttt-box');
+var display = document.querySelector('.display');
 
 
 boxes.forEach((box)=>{
@@ -16,13 +31,25 @@ boxes.forEach((box)=>{
 
 console.log(boxes);
 function handleBoxClick(){
-	if(this.dataset.clicked = 'false'){
-		console.log(this.dataset.pos);
+	if(this.dataset.clicked === 'false'){
+		turnPlayer.moves.push(this.dataset.pos)
+		if(turnPlayer.num === 1){
+			this.classList.add('p1-color');
+		}
+		if(turnPlayer.num === 2){
+			this.classList.add('p2-color');
+		}
+		this.dataset.clicked = 'true';
+		turnPlayer = turnPlayer === player1 ? player2: player1;
+		display.innerHTML = `${turnPlayer.name} , Select a piece `;
 	}
 }
 
 function checkWin(player){
-
+	var myMoves = player.moves.sort((a, b)=>{
+		return a <b;
+	}).join('');
+	console.log(myMoves);
 }
 function game(p1,p2){
 	var turnPlayer = p1;
